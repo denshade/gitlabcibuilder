1a5c1562f7405c081d86b9d88ab964f341d2ac92
denshade <lieven.vaneeckhaute@gmail.com>
2024-06-15 18:10:08 +0200

    simplify.

--- src/main/java/info/thelaboflieven/gitlabci/assertions/GitlabCiAssertions.java
+++ src/main/java/info/thelaboflieven/gitlabci/assertions/GitlabCiAssertions.java
@@ -8,18 +8,14 @@ import javax.script.ScriptException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 public class GitlabCiAssertions {
-    public static Set<String> DEFAULT_STATES = Set.of("build", "test", "deploy");
 
     public static boolean allStagesKnown(GitlabPipeline gitlabPipeline) {
         var declaredStages = new HashSet<>(gitlabPipeline.stages);
         var mentionedStages = gitlabPipeline.gitlabJobList.stream().map(j -> j.stage).collect(Collectors.toSet());
-        var allKnownStates = new HashSet<>(DEFAULT_STATES);
-        allKnownStates.addAll(declaredStages);
-        return allKnownStates.containsAll(mentionedStages);
+        return declaredStages.containsAll(mentionedStages);
     }
 
     public static List<GitlabJob> jobsRunForVariables(GitlabPipeline pipeline, Variable... variables) throws ScriptException {
