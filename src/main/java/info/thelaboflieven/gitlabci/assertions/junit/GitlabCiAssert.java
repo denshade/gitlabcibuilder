0ef5aec265ce98d36b9936e4381412fcd5f09c24
denshade <lieven.vaneeckhaute@gmail.com>
2024-06-16 21:45:11 +0200

    stab at junit assertions.

---
+++ src/main/java/info/thelaboflieven/gitlabci/assertions/junit/GitlabCiAssert.java
@@ -0,0 +1,9 @@
+package info.thelaboflieven.gitlabci.assertions.junit;
+
+import info.thelaboflieven.gitlabci.model.GitlabPipeline;
+
+public class GitlabCiAssert {
+    void assertAllStagesKnown(GitlabPipeline gitlabPipeline) {
+
+    }
+}
